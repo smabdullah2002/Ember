83e71c0517ef258e90b60c028e3bbf574c15db4f
Sheikh Muhammad Abdullah <sheikhmuhammad2002@gmail.com>
2026-03-20 03:06:47 +0600

    added port

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from base import api_router
+import os
+import uvicorn
 
 app = FastAPI()
 
@@ -12,3 +14,8 @@ app.add_middleware(
 )
 
 app.include_router(api_router)
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", "8000"))
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
